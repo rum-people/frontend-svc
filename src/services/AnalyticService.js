ac6c535185b4129a07db478b9863a950869ed422
Nikita Masych <nikitamasich152@gmail.com>
2024-02-11 10:01:43 +0200

    chore: updated constant backend url

--- src/services/AnalyticService.js
+++ src/services/AnalyticService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import consts from "../components/utils/consts";
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'http://upstream';
 
 export default class AnalyticService {
 
